83483819a641cf1b12487a4c597e63e0807b6fc0
hinnaweali <hinnaweali@gmail.com>
2024-11-28 15:41:51 +0100

    fix bug

--- sortArray/sortArray.java
+++ sortArray/sortArray.java
@@ -1,24 +1,22 @@
 import java.util.Arrays;
-int [] sortArray () { 
-	int [] array1 = {3, 7, 4, 11, 8, 5, 2};
-	int counter = 0;
-	for ( int firstNumber = 0; firstNumber< (array1.length) - 1; ++firstNumber) {
-		System.out.println("counter: " + counter);
-		for ( int secondNumber = 0;  secondNumber < array1.length - 1 -firstNumber ; ++secondNumber) {
-			int arrayReplace = 0;
-			if (array1[firstNumber] > array1[secondNumber]) {
-				arrayReplace = array1[firstNumber];
-				//System.out.println(arrayReplace);
-				array1[firstNumber] = array1[secondNumber];
-				System.out.println(array1[firstNumber]);
-				array1[secondNumber] = arrayReplace;
-				System.out.println(array1[secondNumber]);
-				System.out.println(Arrays.toString(array1));
-			}
-		}
-		counter++;
-	}
-	return array1;
+
+int[] sortArray() {
+    int[] array1 = {3, 7, 4, 11, 8, 5, 2};
+    int counter = 0;
+
+    for (int firstNumber = 0; firstNumber < array1.length - 1; ++firstNumber) {
+        System.out.println("counter: " + counter);
+        for (int secondNumber = 0; secondNumber < array1.length - 1 - firstNumber; ++secondNumber) {
+            if (array1[secondNumber] > array1[secondNumber + 1]) {
+                int arrayReplace = array1[secondNumber];
+                array1[secondNumber] = array1[secondNumber + 1];
+                array1[secondNumber + 1] = arrayReplace;
+                System.out.println(Arrays.toString(array1));
+            }
+        }
+        counter++;
+    }
+    return array1;
 }
 
-sortArray();
+System.out.println(Arrays.toString(sortArray()));
