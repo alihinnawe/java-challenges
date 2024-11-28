9e8b3edb937a5356233e75f488aa82970fa2becd
hinnaweali <hinnaweali@gmail.com>
2024-11-28 09:16:41 +0100

    code verbessern

--- ifElse/feirabend.java
+++ ifElse/feirabend.java
@@ -1,28 +1,28 @@
-boolean feirabend (double value1, double value2) {
-	if (value1 > value2) {
+boolean feirabend (double jetzt, double fazeit) {
+	if (jetzt > fazeit) {
 		return true;
 	}
 	else {
+		println ("kein fa"); 
 		return false;
 	}
 }
 
-
-boolean buffetTime (double value2, double value3) {
-
-
-	if (value2 < value3) {
+boolean buffetTime (double jetzt, double buffetzeit) {
+	if (buffetzeit < jetzt) {
 		return true;
 	}
 	else {
+		println ("keine Buffetzeit"); 
 		return false;
 	}
 }
 
-if (feirabend(1,2) && buffetTime(2,3)) {
+// if (feirabend(1,2) && buffetTime(2,3)) {
+// if (feirabend(9,16) && buffetTime(9,12)) { 
+if (feirabend(16,15) && buffetTime(16,12)) {
 	System.out.println("ende");
-} 
-
-else {
+} else {
+	println ("kombi"); 
 	System.out.println("keep working");
 }
