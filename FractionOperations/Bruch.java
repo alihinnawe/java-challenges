765bafa54485ac14cb78a713b775ac0fe1244c63
hinnaweali <hinnaweali@gmail.com>
2024-12-10 15:12:56 +0100

    fabionci plus fractions operations

---
+++ FractionOperations/Bruch.java
@@ -0,0 +1,47 @@
+public class Bruch {
+
+		private int zaehler; 
+		private int nenner; 
+		
+		public Bruch (int zaehler, int nenner)
+		{
+			if (nenner == 0) 
+				throw new ArithmeticException ("Div/0!") ; 
+			this.zaehler = zaehler;
+			this.nenner = nenner; 
+		}
+		
+		public Bruch (int zaehler)
+		{
+				this.zaehler = zaehler;
+				this.nenner = 1; 				
+		}
+		/*
+		public Bruch (int zaehler)
+		{
+				this (zaehler, 1);
+		}
+		*/ 
+		
+		/**
+			(3/4) * (7/5) = (3*7) / (4*5)
+		*/
+		public Bruch mul (Bruch bruch) 
+		{
+				int neuZaehler = zaehler * bruch.zaehler; 
+				int neuNenner = nenner * bruch.nenner; 
+				return new Bruch (neuZaehler, neuNenner); 
+		}
+
+		/**
+			(3/4) + (7/5) =  ((3*5) / (4*5)) + ((7*4) / (5*4))
+						  =  ((3*5) + (7*4)) / (5*4) 
+			(z/n) + (bz/bn)= ((z*bn) + (bz*n)) / (n*bn)  
+		public Bruch add (Bruch b) 
+		{
+				return new Bruch (((zaehler*b.nenner) + (b.zaehler * nenner)), (nnenner * b.nenner)  ); 
+		}
+		*/
+
+}	
+		
