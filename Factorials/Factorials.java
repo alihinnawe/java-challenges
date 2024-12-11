97a9216d3f34d4f0807985856bc5ac77bcc4395b
hinnaweali <hinnaweali@gmail.com>
2024-12-11 11:58:22 +0100

    factorila bug fix

--- Factorials/Factorials.java
+++ Factorials/Factorials.java
@@ -2,20 +2,25 @@ public class Factorials {
 
 		private int factorialNum; 
 		
+		public static int saveFactorial (int num) 
+		{
+			if (num == 1) return 1;
+			return num * saveFactorial(num - 1);
+			}
 		public Factorials ()
 		{
 		}
 		public static void main (String [] arg) {
 			Factorials fact = new Factorials ();
-			int result = fact.getFactorial(7);
+			int result = fact.getFactorial(12);
 			System.out.println("factorial result is: " + result);
 			}
 
 		public int getFactorial (int num) {
-			
+			if (num <0) throw new ArithmeticException ("facotrial of negative numbers is not allowed");
 			if (num <= 1) return 1;
 			
-			return num * getFactorial (num -1);
+			return num * saveFactorial (num -1);
 			}
 
 }	
