cba0e47c30cf6ee4d46ceb27608fa72f8c962c6a
Ali <hinnaweali@gmail.com>
2025-01-15 13:39:24 +0100

    Two panels in main panel

--- Swing/test.java/src/ListDemo.java
+++ Swing/test.java/src/ListDemo.java
@@ -1,29 +1,43 @@
 import java.awt.*;
 import javax.swing.*;
-import java.util.*;
 
 public class ListDemo extends JFrame implements Runnable {
 
-	String [] arr;
-	Vector <String> v;
-	
-	public ListDemo(String title)
-	{   setTitle (title);
-		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-	}
-	
-	
-	public void run ()
-	{
-		JPanel p = new JPanel ();
-		p.setLayout (new BorderLayout ());
-		setVisible(true);
-		pack();
-	}
-	public static void main (String[] args) 
-	{
-		SwingUtilities.invokeLater (
-			new ListDemo ("ListDemo")
-		);
-	}
+    String[] arr = {"Ali", "Hinnawe", "Lebanon", "Beirut"};
+	Vector <String> v =  new Vector <> ();
+	v.add("Name");
+	v.add("FName");
+	v.add("Country");
+	v.add("City");
+
+    public ListDemo(String title) {
+        setTitle(title);
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void run() {
+        JPanel mainPanel = new JPanel();
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+
+        JList<String> jlist1 = new JList<>(arr);
+        JList<String> jlist2 = new JList<>(v);
+
+        p1.add(jlist1);
+        p2.add(jlist2);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(p1, BorderLayout.EAST);
+        mainPanel.add(p2, BorderLayout.WEST);
+
+        add(mainPanel);
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ListDemo("ListDemo").run());
+    }
 }
