a9615dc379b869a001e95c10abc8efe16976e81e
Ali <hinnaweali@gmail.com>
2025-01-15 11:26:42 +0100

    other examples for Layout LEADING na dLEFT

---
+++ Swing/test.java/src/ListDemo.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class ListDemo extends JFrame implements Runnable {
+
+	String [] arr;
+	Vector <String> v;
+	
+	public ListDemo(String title)
+	{   setTitle (title);
+		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	public void run ()
+	{
+		JPanel p = new JPanel ();
+		p.setLayout (new BorderLayout ());
+		setVisible(true);
+		pack();
+	}
+	public static void main (String[] args) {
+		
+		}
+
+}
