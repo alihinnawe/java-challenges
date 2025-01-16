ff30c362a277959aba806b6206db94c4cebdc27a
alihinnawe <hinnaweali@gmail.com>
2025-01-16 22:59:42 +0100

    add checkboxes for all column two and select the checkbox

--- Swing/test.java/src/JCheckbox.java
+++ Swing/test.java/src/JCheckbox.java
@@ -2,15 +2,12 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class JCheckbox extends JFrame {
 
     private Vector<Integer> firstColumn;
     private Vector<String> secondColumn;
     private JPanel checkBoxPanel;
-    private JButton updateButton;
 
     public JCheckbox(String title) {
         super(title);
@@ -21,24 +18,12 @@ public class JCheckbox extends JFrame {
 
         readFile();
 
-        for (int i = 0; i < secondColumn.size(); i++) {
-            JCheckBox checkBox = new JCheckBox(secondColumn.get(i));
+        for (String keyword : secondColumn) {
+            JCheckBox checkBox = new JCheckBox(keyword);
             checkBoxPanel.add(checkBox);
         }
 
-        updateButton = new JButton("Update");
-        updateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                updateFile();
-            }
-        });
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.add(updateButton);
-
         add(new JScrollPane(checkBoxPanel), BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
@@ -66,29 +51,11 @@ public class JCheckbox extends JFrame {
                 lineScanner.close();
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.exit(1);
         }
     }
 
-    private void updateFile() {
-        File file = new File("./keywords.java.txt");
-        try (PrintWriter writer = new PrintWriter(file)) {
-            for (int i = 0; i < checkBoxPanel.getComponentCount(); i++) {
-                JCheckBox checkBox = (JCheckBox) checkBoxPanel.getComponent(i);
-                if (checkBox.isSelected()) {
-                    firstColumn.set(i, 1);
-                } else {
-                    firstColumn.set(i, 0);
-                }
-                writer.println(firstColumn.get(i) + " " + secondColumn.get(i));
-            }
-            JOptionPane.showMessageDialog(this, "File updated successfully!");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new JCheckbox("JCheckbox Example"));
     }
 }
