ebd36749a11994d779c303e6c29ad5607eeef9a8
Ali <hinnaweali@gmail.com>
2025-01-22 10:38:09 +0100

    update the Dialogs example

--- Dialogs/DialogTest.java
+++ Dialogs/DialogTest.java
@@ -3,9 +3,8 @@ import javax.swing.*;
 public class DialogTest extends JFrame {
     Integer a, b;
 
-    public DialogTest(Integer a, Integer b) {
-        this.a = a;
-        this.b = b;
+    public DialogTest() {
+       
         this.setTitle("Dialog Test");
         this.setSize(300, 200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,7 +30,6 @@ public class DialogTest extends JFrame {
         } else {
             System.out.println("Selected option: " + options[selected]);
 
-            if (options[selected].equals("Multiplication")) {
                 String inputA = JOptionPane.showInputDialog(
                     null,
                     "Enter value for 'a':",
@@ -45,51 +43,41 @@ public class DialogTest extends JFrame {
                     "Input for Multiplication",
                     JOptionPane.QUESTION_MESSAGE
                 );
+            a = Integer.parseInt(inputA);
+			b = Integer.parseInt(inputB);
+			if (inputA != null && inputB != null) {
+				if (options[selected].equals("Multiplication")) {
+						int result = a * b;
+						JOptionPane.showMessageDialog(
+							null,
+							"Multiplication result: " + result,
+							"Result",
+							JOptionPane.INFORMATION_MESSAGE
+						);
+                } 
+				 else if (options[selected].equals("Addition")) {
 
-                if (inputA != null && inputB != null) {
-                    int a = Integer.parseInt(inputA);
-                    int b = Integer.parseInt(inputB);
-                    int result = a * b;
+						int result = a + b;
+						JOptionPane.showMessageDialog(
+							null,
+							"Addition result: " + result,
+							"Result",
+							JOptionPane.INFORMATION_MESSAGE
+						);
+				}
+				else {
                     JOptionPane.showMessageDialog(
                         null,
-                        "Multiplication result: " + result,
-                        "Result",
-                        JOptionPane.INFORMATION_MESSAGE
-                    );
-                } else {
-                    JOptionPane.showMessageDialog(
-                        this,
                         "Input canceled. No values provided.",
                         "Warning",
                         JOptionPane.WARNING_MESSAGE
                     );
                 }
-            } else if (options[selected].equals("Addition")) {
-                Object[] possibleValues = { a, b };
-                Object selectedValue = JOptionPane.showInputDialog(
-                    this,
-                    "Choose a value for addition",
-                    "Input",
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    possibleValues,
-                    possibleValues[0]
-                );
-
-                if (selectedValue != null) {
-                    int result = a + b;
-                    JOptionPane.showMessageDialog(
-                        this,
-                        "Addition result: " + result,
-                        "Result",
-                        JOptionPane.INFORMATION_MESSAGE
-                    );
-                }
-            }
+			}
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new DialogTest(5, 4));
+        SwingUtilities.invokeLater(() -> new DialogTest());
     }
 }
