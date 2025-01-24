a1ab0623b331f069ad0141283cd76bf38d7a9d14
Ali Hinnawe <hinnaweali@gmail.com>
2025-01-24 17:30:45 +0100

    add my ow temlate

--- Templates/MyProject/$ClassName.java
+++ Templates/MyProject/$ClassName.java
@@ -1,62 +1,38 @@
-import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
-import java.io.*;
+import java.io.File;
+public class GUIAli extends JFrame implements  Runnable {
+    private static  String name = null;
+    public void run()
+    {
+        File f = new File( "./src/" + name + ".java");
+        try (Scanner sc = new Scanner(f);) {
 
-public class $ClassName extends JFrame implements Runnable {
-    JPanel panel1;
-    JPanel mainPanel;
-    //JComboBox combobox1;
-    //JCheckBox checkBox1;
-    JTextField textFiled1;
-    JTextField textFiled2;
-    JButton button1;
-    JButton button2;
+            System.out.print(sc);
 
-    $ClassName(String s) {
-        super(s);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File not found: " + e.getMessage());
+        }
+
+        Map<String, String> map = new HashMap<>();
+        map.put("\\$NAME", name);
+        map.put("\\$name", name.toLowerCase());
     }
 
-    public void run() {
-        button1 = new JButton("button1");
-        button2 = new JButton("button2");
-        textFiled1 = new JTextField();
-        textFiled2 = new JTextField();
-        textFiled1.setColumns(20);
-        textFiled2.setColumns(20);
-        panel1 = new JPanel();
-        panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
-        panel1.add(button1);
-        panel1.add(button2);
-        panel1.add(textFiled1);
-        panel1.add(textFiled2);
-        panel1.setSize(100,100);
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(panel1, BorderLayout.SOUTH);
-        add (mainPanel);
-        setSize(400, 300);
-        pack();
-        setVisible(true);
-        System.out.println("Welcome to my App");
-    }
-
-    public static void usage() {
-        System.out.print("Java $ClassName ClassName");
-    }
 
     public static void main(String[] args) {
+        name = args[0];
+        System.out.print(name);
+        SwingUtilities.invokeLater(new GUIAli());
 
-        if (args.length == 0) {
-            usage();
-            System.exit(1);
-        } else if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                String Newargs = args[i];
-                System.out.println(Newargs);
-            }
-        }
-        SwingUtilities.invokeLater(new $ClassName("Welcome to my App"));
     }
+
+
+
+
 }
