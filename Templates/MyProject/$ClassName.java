0bd519dc19ed9016f8ffb8e49cf2474129289518
Ali Hinnawe <hinnaweali@gmail.com>
2025-01-24 16:21:34 +0100

    add my ow temlate

--- Templates/MyProject/$ClassName.java
+++ Templates/MyProject/$ClassName.java
@@ -1,37 +1,53 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.*;
+import java.util.Scanner;
+import java.io.*;
 
 public class $ClassName extends JFrame implements Runnable {
-    JPanel panel;
-    JComboBox combobox1;
-    JCheckBox checkBox1;
-    JTextField textFiled1;
+    JPanel panel1;
+    JPanel mainPanel;
+    //JComboBox combobox1;
+    //JCheckBox checkBox1;
+    //JTextField textFiled1;
     JButton button1;
+    JButton button2;
 
-
-    $ClassName (String s){
-        super (s);
+    $ClassName(String s) {
+        super(s);
     }
-    public void run () {
+
+    public void run() {
+        button1 = new JButton("button1");
+        button2 = new JButton("button2");
+        panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        panel1.add(button1);
+        panel1.add(button2);
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(panel1, BorderLayout.SOUTH);
+        add (mainPanel);
+        setSize(400, 300);
+        setVisible(true);
         System.out.println("Welcome to my App");
     }
 
-    public static void usage ()
-    {
+    public static void usage() {
         System.out.print("Java $ClassName ClassName");
     }
 
     public static void main(String[] args) {
 
-        if (args.length == 0)
-        {
+        if (args.length == 0) {
             usage();
             System.exit(1);
+        } else if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                String Newargs = args[i];
+                System.out.println(Newargs);
+            }
         }
         SwingUtilities.invokeLater(new $ClassName("Welcome to my App"));
-
     }
-
 }
