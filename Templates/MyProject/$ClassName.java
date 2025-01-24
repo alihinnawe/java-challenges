60c4589b5dcc082894f23c24fbedb35120408042
Ali Hinnawe <hinnaweali@gmail.com>
2025-01-24 16:17:56 +0100

    add my ow temlate

--- Templates/MyProject/$ClassName.java
+++ Templates/MyProject/$ClassName.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
+
 public class $ClassName extends JFrame implements Runnable {
     JPanel panel;
     JComboBox combobox1;
