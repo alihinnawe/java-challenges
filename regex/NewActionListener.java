05599b1645fd07588035370fd367dd3c2db637ba
Ali <hinnaweali@gmail.com>
2025-01-30 13:37:31 +0100

    update the textarea and fix bugs

--- regex/NewActionListener.java
+++ regex/NewActionListener.java
@@ -7,40 +7,53 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class NewActionListener implements ActionListener{
     JButton button;
     List<JTextField> textFields = new ArrayList<> ();
     List <JTextArea> textAreas =  new ArrayList<> ();
-
+	String inputPath = "C:\\documents\\java-challenges\\regex\\test.txt";
+	String outputPath = "C:\\documents\\java-challenges\\regex\\testNew.txt";
+	
     public NewActionListener(JButton buttonNew,List<JTextField> textFieldNew, List <JTextArea> textAreaNew) {
         button = buttonNew;
         textFields = textFieldNew;
         textAreas = textAreaNew;
     }
     
-    
-    public void load ( String inputPath)
+    public void load ( String inputPath,String outputPath)
     {
-		File f = new File(inputPath);
-		try (Scanner sc = new Scanner(f);){
-			textAreas.get(0).setLineWrap(true);
-			textAreas.get(0).setWrapStyleWord(true);
+		File inF = new File(inputPath);
+		File outF = new File(outputPath);
+		
+		try (Scanner sc = new Scanner(inF);){
+
 			while (sc.hasNext())
-			{   System.out.println("test");
+			{   
 				String nextLine = sc.nextLine();
-				System.out.println("nextLine " + nextLine);
 				textAreas.get(0).append(nextLine + "\n");
 			}
 		} catch (FileNotFoundException e) {
 			System.err.println("Error: File not found: " + e.getMessage());
 		}
-		
+
+		try (Scanner sc = new Scanner(outF);){
+			while (sc.hasNext())
+			{   
+				String nextLine = sc.nextLine();
+				textAreas.get(1).append(nextLine + "\n");
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Error: File not found: " + e.getMessage());
 		}
+	}
+		
 	public void save (String inputPath, String outputPath, String textFvalue1, String textFvalue2) 
 	{
 		File f = new File(inputPath);
 		File newF = new File(outputPath);
+		
 		try (Scanner sc = new Scanner(f); PrintStream ps = new PrintStream(newF)) {
 			while (sc.hasNextLine()) {
 				String nextLine = sc.nextLine();
@@ -52,21 +65,32 @@ public class NewActionListener implements ActionListener{
 		} catch (FileNotFoundException e) {
 			System.err.println("Error: File not found:");
 		}
-
-		try (Scanner sc = new Scanner(newF);){
-			textAreas.get(0).setLineWrap(true);
-			textAreas.get(0).setWrapStyleWord(true);
-			textAreas.get(0).setText("");
-			while (sc.hasNext())
-			{
+		
+		load(inputPath, outputPath);
+	}
+	
+	public void copyToSecond (String textFieldValue1, String textFieldValue2) 
+	{
+		File f = new File (inputPath);
+		try (Scanner sc = new Scanner(f)) {
+			//Boolean replaced = false;
+			while (sc.hasNextLine()) {
 				String nextLine = sc.nextLine();
-				textAreas.get(0).append(nextLine + "\n");
+				//"$1au$2" (.)as(.*)
+				//Pattern compiledPattern = Pattern.compile(textFieldValue1);
+				//Matcher matcher = compiledPattern.matcher(nextLine);
 
+				//if (matcher.matches()) {
+				String nextLineCopy = nextLine.replaceAll(textFieldValue1,textFieldValue2);
+				textAreas.get(1).append(nextLineCopy +"\n");
+				//}
+				 
 			}
 		} catch (FileNotFoundException e) {
-			System.err.println("Error: File not found: " + e.getMessage());
+			System.err.println("Error: File not found:");
 		}
 	}
+		
 		
 		
     public void actionPerformed(ActionEvent ae) {
@@ -81,7 +105,7 @@ public class NewActionListener implements ActionListener{
             if(returnVal == JFileChooser.APPROVE_OPTION) {
                 System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
             }
-            load("C:\\documents\\java-challenges\\regex\\test.txt");
+            load(inputPath, outputPath);
         }
         else if (cmd.equals("save")){
             System.out.print("button save true");
@@ -97,9 +121,9 @@ public class NewActionListener implements ActionListener{
             String txtFValue2 = textFields.get(1).getText();
 			//"C:\\documents\\java-challenges\\regex\\test.txt"
 			//"C:\\documents\\java-challenges\\regex\\testCorrected.txt"
-			String inputP = "C:\\documents\\java-challenges\\regex\\test.txt";
-			String outputP = "C:\\documents\\java-challenges\\regex\\testCorrected.txt";
-			save(inputP, outputP,txtFValue1, txtFValue2);
+			//save(inputPath, outputPath,txtFValue1, txtFValue2);
+			//load(inputPath, outputPath);
+			copyToSecond(txtFValue1,txtFValue2);
         }
         else {
             String btnText = button.getText();
